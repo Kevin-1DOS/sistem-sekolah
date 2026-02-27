3790af7ffdd26613569e8981b3296819ad9b02ca
Kevin-1DOS <kevinasdw124@gmail.com>
2026-02-27 11:31:57 +0700

    feat: setuo tailwind

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -6,14 +6,14 @@ namespace App\Controllers;
 class StudentController {
 
 public function index() {
-require_once './app/views/students/index.php';
+require_once '../app/views/students/index.php';
 }
 
 public function create() {
-require_once './app/views/students/create.php';
+require_once '../app/views/students/create.php';
 }
 
 public function show($id) {
-require_once './app/views/students/show.php';
+require_once '../app/views/students/show.php';
 }
 }
