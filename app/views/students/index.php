11bcd8faec8634593f489f73f715657c54a41a14
Kevin-1DOS <kevinasdw124@gmail.com>
2026-03-31 07:55:36 +0700

    feat: implement students index page

--- app/views/students/index.php
+++ app/views/students/index.php
@@ -3,32 +3,66 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/output.css">
     <title>Daftar Siswa</title>
+    <link rel="stylesheet" href="/css/output.css">
 </head>
-<body class="bg-gray-200 min-h-screen flex flex-col">
+<body class="min-h-screen flex flex-col bg-gray-100">
     <header class="bg-blue-500 text-white">
-        <div class="flex items-center justify-between container mx-auto p-4 ">
+        <div class="flex items-center justify-between container mx-auto p-4">
             <a href="/students" class="font-bold text-xl">Sistem Sekolah</a>
-            <a href="/students/create.php" class="bg-white text-blue-500 px-4 py-2 rounded-lg">+ Tambah Siswa</a>
+            <a href="/students/create" class="bg-white text-blue-500 px-4 py-2 rounded-lg">+ Tambah Siswa</a>
         </div>
     </header>
-
-
-    <Main class="grow container mx-auto">
-        <div class="mt-10">
-            <div class="bg-white shadow rounded-lg p-4  ">
-                <h1 class="text-xl font-bold">Daftar Siswa</h1>
-                <p class="text-lg">Menampilkan Daftar Siswa yang Terdaftar</p>
+ 
+    <main class="grow container mx-auto">
+        <div class="mt-8 space-y-4">
+            <!--Card header start-->
+            <div class="bg-white shadow rounded-lg p-4">
+                <h1 class="text-2xl font-bold">Daftar Siswa</h1>
+                <p>Menampilkan daftar siswa yang terdaftar</p>
             </div>
-        </div>
+            <!--Card header end-->
 
-      
-
-    <footer class="bg-green-800 text-white">
-        <div class="text-center p-4">
-        &copy; <?= date(format:"Y")?> - Sistem Sekolah - SMK Kristen Immanuel
+            <!--Card content start-->
+            <div class="bg-white-200 shadow rounded-lg">
+                <table class="w-full">
+                    <thead class="bg-green-200">
+                        <tr>
+                            <th class="py-2 px-4 text-left">No</th>
+                            <th class="py-2 px-4 text-left">Nama</th>
+                            <th class="py-2 px-4 text-left">NIS</th>
+                            <th class="py-2 px-4 text-left">Kelas</th>
+                            <th class="py-2 px-4 text-left">No Telepon</th>
+                            <th class="py-2 px-4">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="py-2 px-4 text-left">1</td>
+                            <td class="py-2 px-4 text-left">Andi</td>
+                            <td class="py-2 px-4 text-left">1234</td>
+                            <td class="py-2 px-4 text-left">11 TKJ 3</td>
+                            <td class="py-2 px-4 text-left">0812345678</td>
+                            <td class="py-2 px-4">
+                                <div class="flex items-center justify-center gap-14">
+                                    <a href="" class="text-green-500">Detail</a>
+                                    <a href="" class="text-yellow-500">Edit</a>
+                                    <a href="" class="text-red-500">Hapus</a>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <!--Card content end-->
         </div>
+    </main>
+ 
+    <footer class="bg-green-800 text-white item">
+            <div class="text-center p-4">
+                &copy <?= date(format: "Y") ?> - Sistem Sekolah SMK Kristen Immanuel
+            </div>
     </footer>
+ 
 </body>
 </html>
