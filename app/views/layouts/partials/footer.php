9d70e6b2665160efe9c955473ae5504711046226
Kevin-1DOS <kevinasdw124@gmail.com>
2026-04-10 10:56:06 +0700

    feat:implement layout

---
+++ app/views/layouts/partials/footer.php
@@ -0,0 +1,5 @@
+<footer class="bg-green-800 text-white item">
+            <div class="text-center p-4">
+                &copy <?= date(format: "Y") ?> - Sistem Sekolah SMK Kristen Immanuel
+            </div>
+    </footer>
